8eb1ec06fed4a2217778a3c56623cfce74cc33f7
eesss34690 <55132736+eesss34690@users.noreply.github.com>
2020-05-16 21:22:24 +0800

    Add files via upload

--- index.cpp
+++ index.cpp
@@ -8,24 +8,15 @@
 #define OUTPUT_FILE "key_query_out.txt"
 #define OUT2 "range_query_out.txt"
 
-bool Node :: Get_IsLeaf()
+Node :: Node()
 {
-	return isLeaf;
+	isLeaf = true;
+	prev = this;
+	next = this;
 }
 
 
-vector<int> Node :: Get_Keys()
-{
-	return keys;
-}
-
-InternalNode :: InternalNode()
-{
-	isLeaf = false;
-}
-
-
-void InternalNode :: Insertkn(int key, Node* r)
+void Node :: Insertkn(int key, Node* r)
 {
 	vector<int>::iterator index = lower_bound(keys.begin(), keys.end(), key);
 	keys.insert(index, key);
@@ -35,7 +26,7 @@ void InternalNode :: Insertkn(int key, Node* r)
 }
 
 
-void InternalNode :: Insertknn(int key, Node* l, Node* r)
+void Node :: Insertknn(int key, Node* l, Node* r)
 {
 	keys.push_back(key);
 	children.push_back(l);
@@ -43,47 +34,24 @@ void InternalNode :: Insertknn(int key, Node* l, Node* r)
 }
 
 
-Node* InternalNode :: Split(int* kp)
+Node* Node :: Split(int* kp)
 {
-	int length = keys.size();
+    if (!this->isLeaf)
+    {
+        int length = keys.size();
 
-	InternalNode* r = new InternalNode;
+        Node* r = new Node;
+        r->isLeaf = false;
+        *kp = keys[length/2];
 
-	*kp = keys[length/2];
+        r->keys.assign(keys.begin() + (length/2 + 1), keys.end());
+        r->children.assign(children.begin() + (length/2 + 1), children.end());
+        keys.erase(keys.begin() + length/2, keys.end());
+        children.erase(children.begin() + (length/2 + 1), children.end());
 
-	r->keys.assign(keys.begin() + (length/2 + 1), keys.end());
-	r->children.assign(children.begin() + (length/2 + 1), children.end());
-	keys.erase(keys.begin() + length/2, keys.end());
-	children.erase(children.begin() + (length/2 + 1), children.end());
-
-	return r;
-}
-
-vector<Node*> InternalNode :: Get_Children()
-{
-	return children;
-}
-
-LeafNode :: LeafNode()
-{
-	isLeaf = true;
-	prev = this;
-	next = this;
-}
-
-void LeafNode :: Insertkv(int key, int value)
-{
-	vector<int>::iterator index = lower_bound(keys.begin(), keys.end(), key);
-
-    keys.insert(index, key);
-    index = lower_bound(keys.begin(), keys.end(), key);
-    values.insert(values.begin() + (index - keys.begin()), value);
-}
-
-
-Node* LeafNode :: Split(int* kp)
-{
-	LeafNode* r = new LeafNode;
+        return r;
+    }
+	Node* r = new Node;
 
 	*kp = keys[keys.size()/2];
 
@@ -98,49 +66,43 @@ Node* LeafNode :: Split(int* kp)
 	(r->next)->prev = r;
 
 	return r;
+
 }
 
-
-vector<int> LeafNode :: Get_Values()
+void Node :: Insertkv(int key, int value)
 {
-	return values;
+	vector<int>::iterator index = lower_bound(keys.begin(), keys.end(), key);
+
+    keys.insert(index, key);
+    index = lower_bound(keys.begin(), keys.end(), key);
+    values.insert(values.begin() + (index - keys.begin()), value);
 }
-
-
-Node* LeafNode :: Get_Next()
-{
-	return next;
-}
-
 
 void Index :: Search_Path(Node* node, int key, stack<Node*>* path)
 {
     path->push(node);
 
-	if(!node->Get_IsLeaf())
+	if(!node->isLeaf)
     {
-		vector<int> keys = node->Get_Keys();
-		vector<Node*> children = node->Get_Children();
-        vector<int>::iterator index = lower_bound(keys.begin(), keys.end(), key);
+        vector<int>::iterator index = lower_bound(node->keys.begin(), node->keys.end(), key);
 
-		if(key == keys[index - keys.begin()])
+		if(key == node->keys[index - node->keys.begin()])
 		{
-            Search_Path(children[(index - keys.begin()) + 1], key, path);
+            Search_Path(node->children[(index - node->keys.begin()) + 1], key, path);
 		}
 
 		else
 		{
-			Search_Path(children[index - keys.begin()], key, path);
+			Search_Path(node->children[index - node->keys.begin()], key, path);
 		}
     }
 }
 
 void Index :: Destroy(Node* node)
 {
-	if (!node->Get_IsLeaf())
+	if (!node->isLeaf)
 	{
-        vector<Node*> c = node->Get_Children();
-        for(vector<Node*>::iterator index = c.begin(); index != c.end(); index++)
+        for(vector<Node*>::iterator index = node->children.begin(); index != node->children.end(); index++)
         {
             Destroy(*index);
         }
@@ -148,104 +110,64 @@ void Index :: Destroy(Node* node)
 	delete(node);
 }
 
-void Index :: Initialize()
-{
-	root = NULL;
-}
-
 Index :: Index(int num, vector<int> keys, vector<int> val)
 {
 
     vector<int>::iterator iter2= val.begin();
-    Initialize();
+    root = NULL;
     for (vector<int>::iterator iter1= keys.begin(); iter1!= keys.end(); iter1++)
     {
-        Insert(*iter1, *iter2);
+        if(NULL == root)
+        {
+            root = new Node;
+            root->Insertkv(*iter1, *iter2);
+        }
+
+        else
+        {
+            Node* l = new Node;
+            l= NULL;
+            Node* r = new Node;
+            r = NULL;
+            int* kp =new int;
+            bool _pop = false;
+
+            stack<Node*>* path = new stack<Node*>;
+            Search_Path(root, *iter1, path);
+            path->top()->Insertkv(*iter1, *iter2);
+            while(path->top()->keys.size() == order)
+            {
+                l = path->top();
+                r = l->Split(kp);
+                if (path->size()> 0)path->pop();
+                if(!path->empty())
+                {
+                    path->top()->Insertkn(*kp, r);
+                }
+
+                else
+                {
+                    _pop = true;
+                    break;
+                }
+            }
+
+            if(_pop)
+            {
+                Node* temp = new Node;
+                temp->isLeaf = false;
+                temp->Insertknn(*kp, l, r);
+
+                root = temp;
+            }
+            delete(kp);
+            delete(path);
+        }
         iter2++;
     }
 }
 
-void Index :: Insert(int key, int value)
-{
-	if(NULL == root)
-	{
-		root = new LeafNode;
-		root->Insertkv(key, value);
-	}
-
-	else
-	{
-		Node* l = new Node;
-		l= NULL;
-		Node* r = new Node;
-		r = NULL;
-		int* kp =new int;
-		bool _pop = false;
-
-		stack<Node*>* path = new stack<Node*>;
-		Search_Path(root, key, path);
-		path->top()->Insertkv(key, value);
-		while(path->top()->Get_Keys().size() == order)
-		{
-			l = path->top();
-			r = l->Split(kp);
-			if (path->size()> 0)path->pop();
-			if(!path->empty())
-			{
-				path->top()->Insertkn(*kp, r);
-			}
-
-			else
-			{
-				_pop = true;
-				break;
-			}
-		}
-
-		if(_pop)
-		{
-			InternalNode* temp = new InternalNode;
-
-			temp->Insertknn(*kp, l, r);
-
-			root = temp;
-		}
-		delete(kp);
-		delete(path);
-	}
-}
-
-
-int Index :: Search1(int key)
-{
-	if(NULL == root)
-	{
-		return -1;
-	}
-    int i = 0;
-
-    stack<Node*>* path = new stack<Node*>;
-    Search_Path(root, key, path);
-
-    vector<int> keys = path->top()->Get_Keys();
-    vector<int> values = path->top()->Get_Values();
-    vector<int>::iterator index = lower_bound(keys.begin(), keys.end(), key);
-
-    if(key == keys[index - keys.begin()])
-    {
-        return values[index - keys.begin()];
-    }
-
-    else
-    {
-        return -1;
-    }
-
-    delete(path);
-}
-
-
-vector<int> Index :: Search2(int key1, int key2)
+vector<int> Index :: Search(int key1, int key2)
 {
     vector<int> a;
 	if(NULL == root)
@@ -262,26 +184,26 @@ vector<int> Index :: Search2(int key1, int key2)
 		Search_Path(root, key1, path);
 		Search_Path(root, key2, path2);
 
-		vector<int> keys = path->top()->Get_Keys();
-		vector<int> keys2 = path2->top()->Get_Keys();
-		vector<int> values = path->top()->Get_Values();
-		Node* next = new Node;
-		next = path->top()->Get_Next();
-		vector<int>::iterator index = lower_bound(keys.begin(), keys.end(), key1);
+		vector<int> key = path->top()->keys;
+		vector<int> keys2 = path2->top()->keys;
+		vector<int> v = path->top()->values;
+		Node* n = new Node;
+		n = path->top()->next;
+		vector<int>::iterator index = lower_bound(key.begin(), key.end(), key1);
         vector<int>::iterator index2 = lower_bound(keys2.begin(), keys2.end(), key2);
 
             while(1)
             {
-                if((index - keys.begin()) == keys.size())
+                if((index - key.begin()) == key.size())
                 {
-                    keys = next->Get_Keys();
-                    values = next->Get_Values();
-                    next = next->Get_Next();
-                    index = keys.begin();
+                    key = n->keys;
+                    v = n->values;
+                    n = n->next;
+                    index = key.begin();
                 }
-                if((key1 <= keys[index - keys.begin()]) && (keys[index - keys.begin()] <= key2))
+                if((key1 <= key[index - key.begin()]) && (key[index - key.begin()] <= key2))
                 {
-                    a.push_back(values[index- keys.begin()]);
+                    a.push_back(v[index- key.begin()]);
                 }
                 else
                 {
@@ -300,10 +222,35 @@ vector<int> Index :: Search2(int key1, int key2)
 void Index :: key_query(vector<int> query_keys)
 {
     fstream output;
+    vector<int> k, v;
+    vector<int>::iterator index;
     output.open(OUTPUT_FILE, ios::out | ios::trunc);
     for (int i= 0; i< query_keys.size(); i++)
     {
-        output<<Search1(query_keys[i])<<endl;
+        if(NULL == root)
+        {
+            output<<"-1"<<endl;
+            continue;
+        }
+
+        stack<Node*>* path = new stack<Node*>;
+        Search_Path(root, query_keys[i], path);
+
+        k = path->top()->keys;
+        v = path->top()->values;
+        index = lower_bound(k.begin(), k.end(), query_keys[i]);
+
+        if(query_keys[i] == k[index - k.begin()])
+        {
+            output<<v[index - k.begin()]<<endl;
+        }
+
+        else
+        {
+            output<<"-1"<<endl;
+        }
+
+        delete(path);
     }
     output.close();
 }
@@ -314,7 +261,7 @@ void Index :: range_query(vector<pair<int,int>> query_pairs)
     vector<int> a;
     for (int i= 0 ; i< query_pairs.size(); i++)
     {
-        a = Search2(query_pairs[i].first, query_pairs[i].second);
+        a = Search(query_pairs[i].first, query_pairs[i].second);
         if (a.size()== 1&& a[0]== -1)
         {
             output2<<"-1\n";
--- index.h
+++ index.h
@@ -12,73 +12,37 @@
 using namespace std;
 class Node
 {
-	protected:
+	public:
 		bool isLeaf;
 		vector<int> keys;
-
-	public:
-		bool Get_IsLeaf();
-		vector<int> Get_Keys();
-		virtual void Insertkv(int key, int value){}
-		virtual void Insertkn(int key, Node* rightChild){}
-		virtual void Insertknn(int key, Node* leftChild, Node* rightChild){}
-		virtual void Search(int key){}
-		virtual void Search(int key1, int key2){}
-		virtual Node* Split(int* keyToParent){}
-		virtual vector<Node*> Get_Children(){}
-		virtual vector<int> Get_Values(){}
-		virtual Node* Get_Next(){}
-};
-
-
-// internal node
-class InternalNode : public Node
-{
-	private:
 		vector<Node*> children;
-
-	public:
-		InternalNode();
-		void Insertkn(int key, Node* rightChild);
-		void Insertknn(int key, Node* leftChild, Node* rightChild);
-		Node* Split(int* keyToParent);
-		vector<Node*> Get_Children();
-};
-
-
-// leaf node
-class LeafNode : public Node
-{
-	private:
-		LeafNode* prev;
-		LeafNode* next;
+		Node* prev;
+		Node* next;
 		vector<int> values;
 
 	public:
-		LeafNode();
+		Node();
+		void Insertkn(int key, Node* rightChild);
+		void Insertknn(int key, Node* leftChild, Node* rightChild);
+		Node* Split(int* kp);
 		void Insertkv(int key, int value);
-		Node* Split(int* keyToParent);
-		vector<int> Get_Values();
-		Node* Get_Next();
 };
 
 
 // B+ tree
 class Index
 {
-	private:
-		int order= 50;
+	public:
+		int order= 40;
 		Node* root;
 		ofstream outputFile;
 		void Search_Path(Node* node, int key, stack<Node*>* path);
 		void Destroy(Node* node);
 
 	public:
-		void Initialize();
 		Index(int num, vector<int> keys, vector<int> val);
 		void Insert(int key, int value);
-		int Search1(int key);
-		vector<int> Search2(int key1, int key2);
+		vector<int> Search(int key1, int key2);
 		void key_query(vector<int> query_keys);
 		void range_query(vector<pair<int,int>> query_pairs);
 		void clear_index();
